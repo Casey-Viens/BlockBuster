dc81434597d5da5593bf33eb7e2f797f3ea4b98d
Casey Viens <ceviens@email.neit.edu>
2022-02-01 04:27:57 -0500

    Class work console

---
+++ BlockBusterConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlockBuster;
+
+namespace BlockBusterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = BlockBusterBasicFunctions.GetAllMovies();
+            var oh = new OutputHelper();
+            oh.WriteToCSV(b);
+        }
+
+    }
+}
