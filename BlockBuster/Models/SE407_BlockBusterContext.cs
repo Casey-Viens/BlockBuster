3d041ff8ecae04b0a2bae6a5b03e4b98dd8dbc7e
Casey Viens <ceviens@email.neit.edu>
2022-02-01 17:31:37 -0500

    Moved the SQL Connection string to environment variables to keep it out of repo

--- BlockBuster/Models/SE407_BlockBusterContext.cs
+++ BlockBuster/Models/SE407_BlockBusterContext.cs
@@ -28,7 +28,9 @@ namespace BlockBuster.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=sql.neit.edu,4500;User Id=SE407_BlockBuster;Password=Bl0ckBu$t3r;Database=SE407_BlockBuster;");
+                // SQL Connection String is stored in EnvironmentVariables as SQLServerConnection
+                string value = Environment.GetEnvironmentVariable("SQLServerConnection");
+                optionsBuilder.UseSqlServer($"{value}");
             }
         }
 
