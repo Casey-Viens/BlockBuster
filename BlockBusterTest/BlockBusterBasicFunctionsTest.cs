bb28984588cac08b41faa598e46f19a3da509ff5
Casey Viens <ceviens@email.neit.edu>
2022-02-01 01:36:27 -0500

    Add project files.

---
+++ BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BlockBuster;
+namespace BlockBusterTest
+{
+    public class BlockBusterBasicFunctionsTest
+    {
+
+        [Fact]
+        public void GetMovieByIDTest()
+        {
+            var result = BlockBusterBasicFunctions.GetMovieByID(11);
+            Assert.True(result.Title == "Vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+        }
+
+        [Fact]
+        public void GetAllMoviesTest()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMovies();
+            Assert.True(result.Count == 49);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMovies()
+        {
+            var result = BlockBusterBasicFunctions.GetAllCheckedOutMovies();
+            Assert.True(result.Count == 3);
+        }
+
+    }
+}
