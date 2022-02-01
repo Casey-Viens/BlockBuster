71964e33caf0df4f058ca529b01843885536030e
Casey Viens <ceviens@email.neit.edu>
2022-02-01 04:36:00 -0500

    Outputs Argument Homework

--- BlockBusterConsole/Program.cs
+++ BlockBusterConsole/Program.cs
@@ -13,7 +13,21 @@ namespace BlockBusterConsole
         {
             var b = BlockBusterBasicFunctions.GetAllMovies();
             var oh = new OutputHelper();
-            oh.WriteToCSV(b);
+
+            args = Environment.GetCommandLineArgs();
+
+            if (args[1].ToLower() == "console")
+            {
+                oh.WriteToConsole(b);
+            }else if(args[1].ToLower() == "csv")
+            {
+                oh.WriteToCSV(b);
+            }
+            else
+            {
+                Console.WriteLine($"Desired output is not available.");
+                Environment.Exit(99);
+            }
         }
 
     }
